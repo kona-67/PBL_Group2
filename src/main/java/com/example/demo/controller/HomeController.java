2158905370de8e23abcfc5be1eb0ef8747df1dcd
moriyasu <25110036@oic-ok.ac.jp>
2026-06-18 09:40:18 +0900

    HomeController.javaにMedicineDBから情報を引っ張ってくる機能を追加しました。

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,15 +1,32 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.entity.Medicine;
+import com.example.demo.repository.MedicineRepository;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    private MedicineRepository medicineRepository;
+
     // 1. ホーム画面を表示する
     @GetMapping("/home")
-    public String showHomePage() {
-    return "home";
+    public String showHomePage(Model model) {
+
+        // ★RDSから全データを取ってくる（セレクトSQLが自動で飛ぶ）
+        List<Medicine> medicineList = medicineRepository.findAll();
+
+        // ★HTMLにデータを渡す
+        model.addAttribute("medicineList", medicineList);
+
+        return "home"; // templates/home.html を開く
     }
 
     // 1. ログイン画面を表示する
@@ -34,6 +51,8 @@ public class HomeController {
     @GetMapping("/history") 
     public String showHistoryPage() {
         return "history"; // templates/history.html を開く
-    }    
+    }
+
+
 
 }
